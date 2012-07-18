99c5ea8b35174b65fd78c5b8e689c325c555be2a
bdaun1@gmail.com <bdaun1@gmail.com@db50c294-acef-6d43-bc94-4116f952d554>
2012-07-18 02:03:49 +0000

    Added "Show Shipped" option checkbox.  Changes to aspx and cs

--- trunk/IMDBWeb/Secure/SPAKpages/TechProg.aspx.designer.cs
+++ trunk/IMDBWeb/Secure/SPAKpages/TechProg.aspx.designer.cs
@@ -121,6 +121,15 @@ namespace IMDBWeb.Secure.SPAKpages {
         protected global::System.Web.UI.UpdateProgress progGridView;
         
         /// <summary>
+        /// chkShipped control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkShipped;
+        
+        /// <summary>
         /// gvTechName control.
         /// </summary>
         /// <remarks>
