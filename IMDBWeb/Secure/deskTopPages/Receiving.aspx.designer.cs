cce7b4e0d75bb1313c77341dd95ac6b970b13497
bdaun1@gmail.com <bdaun1@gmail.com@db50c294-acef-6d43-bc94-4116f952d554>
2012-08-09 17:13:29 +0000

    formatting changes only

--- IMDBWeb/Secure/deskTopPages/Receiving.aspx.designer.cs
+++ IMDBWeb/Secure/deskTopPages/Receiving.aspx.designer.cs
@@ -211,6 +211,15 @@ namespace IMDBWeb.Secure.SPAKpages {
         protected global::System.Web.UI.WebControls.DetailsView dvHdrDetail;
         
         /// <summary>
+        /// sdsContainerList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource sdsContainerList;
+        
+        /// <summary>
         /// sdsHdrDetail control.
         /// </summary>
         /// <remarks>
