dc3043186af8a665291e27ebfac525be545d1bdc
bdaun1@gmail.com <bdaun1@gmail.com@db50c294-acef-6d43-bc94-4116f952d554>
2012-10-17 13:35:45 +0000

    Add Issues and Ideas link at bottom of page

--- IMDBWeb/Site.Master.designer.cs
+++ IMDBWeb/Site.Master.designer.cs
@@ -67,6 +67,15 @@ namespace IMDBWeb {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
         
         /// <summary>
+        /// hypIdea control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypIdea;
+        
+        /// <summary>
         /// Clear control.
         /// </summary>
         /// <remarks>
