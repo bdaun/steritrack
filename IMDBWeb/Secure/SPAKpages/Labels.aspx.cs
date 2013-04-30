5bfbe61dedff98f36c270de5716898e46053fa30
bdaun1@gmail.com <bdaun1@gmail.com@db50c294-acef-6d43-bc94-4116f952d554>
2013-04-30 03:44:55 +0000

    Create labels functionality complete

--- IMDBWeb/Secure/SPAKpages/Labels.aspx.cs
+++ IMDBWeb/Secure/SPAKpages/Labels.aspx.cs
@@ -20,11 +20,13 @@ namespace IMDBWeb.Secure.SPAKpages
                 truckrow1.Visible = false;
                 truckrow2.Visible = false;
                 truckrow3.Visible = false;
+                lblCreateMsg.Visible = false;
             }
         }
 
         protected void txbTruckDate_TextChanged(object sender, EventArgs e)
         {
+            lblCreateMsg.Visible = false;
             if (!Regex.IsMatch(txbTruckDate.Text, @"^(0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])[/][01]\d$"))
             {
                 WebMsgBox.Show("Please use the format of mm/dd/yy");
@@ -35,6 +37,7 @@ namespace IMDBWeb.Secure.SPAKpages
 
         protected void txbNumberContainers_TextChanged(object sender, EventArgs e)
         {
+            lblCreateMsg.Visible = false;
             if (!Regex.IsMatch(txbNumberContainers.Text, @"^[0-9]{1,3}$"))
             {
                 WebMsgBox.Show("Please enter a value between 1 and 999");
@@ -45,33 +48,33 @@ namespace IMDBWeb.Secure.SPAKpages
 
         protected void btnCreateLabel_Click(object sender, EventArgs e)
         {
-            //check that values have been selected
-            if (string.IsNullOrEmpty(txbNumberContainers.Text) || string.IsNullOrWhiteSpace(txbNumberContainers.Text) || Convert.ToInt32(txbNumberContainers.Text) < 1)
+            if (ddCreateItems.SelectedValue.ToString() == "TruckTag")
             {
-                WebMsgBox.Show("Please enter a valid number of containers");
-                txbNumberContainers.Focus();
-            }
-            else if (ddSiteSelect.SelectedIndex == 0)
-            {
-                WebMsgBox.Show("Please select a site from the dropdown list");
-                ddSiteSelect.Focus();
-            }
-            else if (string.IsNullOrEmpty(txbTruckDate.Text) || string.IsNullOrWhiteSpace(txbTruckDate.Text))
-            {
-                WebMsgBox.Show("Please enter a valid date for the truck arrival");
-                txbTruckDate.Focus();
-            }
-            else if (string.IsNullOrEmpty(txbTruckSeqNumber.Text) || string.IsNullOrWhiteSpace(txbTruckSeqNumber.Text))
-            {
-                WebMsgBox.Show("Please enter a truck sequence number in the format of ###");
-                txbTruckSeqNumber.Focus();
-            }
-            else
-            {
+                //check that values have been selected
+                if (string.IsNullOrEmpty(txbNumberContainers.Text) || string.IsNullOrWhiteSpace(txbNumberContainers.Text) || Convert.ToInt32(txbNumberContainers.Text) < 1)
+                {
+                    WebMsgBox.Show("Please enter a valid number of containers");
+                    txbNumberContainers.Focus();
+                }
+                else if (ddSiteSelect.SelectedIndex == 0)
+                {
+                    WebMsgBox.Show("Please select a site from the dropdown list");
+                    ddSiteSelect.Focus();
+                }
+                else if (string.IsNullOrEmpty(txbTruckDate.Text) || string.IsNullOrWhiteSpace(txbTruckDate.Text))
+                {
+                    WebMsgBox.Show("Please enter a valid date for the truck arrival");
+                    txbTruckDate.Focus();
+                }
+                else if (string.IsNullOrEmpty(txbTruckSeqNumber.Text) || string.IsNullOrWhiteSpace(txbTruckSeqNumber.Text))
+                {
+                    WebMsgBox.Show("Please enter a truck sequence number in the format of ###");
+                    txbTruckSeqNumber.Focus();
+                }
+                else
+                {
                 //Create labels
 
-                if (ddCreateItems.SelectedValue.ToString() == "TruckTag")
-                {
                     int lblCounter = 1;
                     int initLblCnt = 0;
                     String spLblCnt = "SPAK_Labels_CountLabels_Sel";
@@ -135,9 +138,24 @@ namespace IMDBWeb.Secure.SPAKpages
                             {
                                 spCmdLblIns.Parameters.Clear();
                                 con.Close();
+                                lblCreateMsg.Visible = true;
+  
                             }
                         }
+                        txbNumberContainers.Text = string.Empty;
+                        ddSiteSelect.SelectedIndex = 0;
+                        txbTruckDate.Text = string.Empty;
+                        txbTruckSeqNumber.Text = "001";
                     }
+                }
+            }
+            else
+            {
+                //check that values have been selected
+                if (string.IsNullOrEmpty(txbNumberContainers.Text) || string.IsNullOrWhiteSpace(txbNumberContainers.Text) || Convert.ToInt32(txbNumberContainers.Text) < 1)
+                {
+                    WebMsgBox.Show("Please enter a valid number of containers");
+                    txbNumberContainers.Focus();
                 }
                 else
                 {
@@ -185,10 +203,9 @@ namespace IMDBWeb.Secure.SPAKpages
                     using (spCmdTagIns)
                     {
                         int TagCounter = 1;
-                        int TagValue = 100000000;
+                        int TagValue = 100000000 + TagCount + TagCounter;  // Adding in the 100000000 creates consistent Tag length (for a while anyway)
                         while (TagCounter <= Convert.ToInt32(txbNumberContainers.Text))
-                        {
-                            TagValue = TagCounter + TagCount + TagValue;
+                        { 
                             String CntrID = TagType + "-" + TagValue;
                             con.Open();
                             try
@@ -196,6 +213,7 @@ namespace IMDBWeb.Secure.SPAKpages
                                 spCmdTagIns.Parameters.AddWithValue("@CntrID", CntrID);
                                 spCmdTagIns.Parameters.AddWithValue("@Type", ddCreateItems.SelectedValue.ToString());
                                 spCmdTagIns.Parameters.AddWithValue("@UserName", HttpContext.Current.User.Identity.Name.ToString());
+                                spCmdTagIns.ExecuteNonQuery();
 
                             }
                             catch (Exception ex)
@@ -205,17 +223,25 @@ namespace IMDBWeb.Secure.SPAKpages
                             }
                             finally
                             {
+                                TagCounter = TagCounter + 1;
+                                TagValue = TagValue + 1;
                                 spCmdTagIns.Parameters.Clear();
                                 con.Close();
+                                lblCreateMsg.Visible = true;
                             }
                         }
+                        txbNumberContainers.Text = string.Empty;
+                        ddSiteSelect.SelectedIndex = 0;
+                        txbTruckDate.Text = string.Empty;
+                        txbTruckSeqNumber.Text = "001";
                     }
-                }
+                }  
             }
         }
 
         protected void txbTruckSeqNumber_TextChanged(object sender, EventArgs e)
         {
+            lblCreateMsg.Visible = false;
             if (!Regex.IsMatch(txbTruckSeqNumber.Text, @"^\d\d\d$"))
             {
                 WebMsgBox.Show("Please enter a 3 Digit sequence number.  For example, '002'.");
@@ -226,6 +252,12 @@ namespace IMDBWeb.Secure.SPAKpages
 
         protected void ddCreateItems_SelectedIndexChanged(object sender, EventArgs e)
         {
+            lblCreateMsg.Visible = false;
+            txbNumberContainers.Text = string.Empty;
+            ddSiteSelect.SelectedIndex = 0;
+            txbTruckDate.Text = string.Empty;
+            txbTruckSeqNumber.Text = "001";
+
             if (ddCreateItems.SelectedIndex == 0)
             {
                 tblCreateLabels.Visible = false;
