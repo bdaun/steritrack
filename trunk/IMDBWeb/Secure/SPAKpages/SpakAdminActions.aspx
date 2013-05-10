f184f176dfb36e45d78d84ecdbed428683632add
bdaun1@gmail.com <bdaun1@gmail.com@db50c294-acef-6d43-bc94-4116f952d554>
2013-05-10 21:23:34 +0000

    Initial Release

---
+++ trunk/IMDBWeb/Secure/SPAKpages/SpakAdminActions.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SpakAdminActions.aspx.cs" Inherits="IMDBWeb.Secure.SPAKpages.SpakAdminActions" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+<h3>SPAK Data Admin Actions</h3>
+<hr />
+<asp:Label ID="lblErrMsg" runat="server" Font-Bold="True" Font-Size="Large" ForeColor="Red" />
+    <table border="2" cellpadding="10" style="background-color:#f9f9f9">
+        <tr style="text-align:center;font-size:x-large;font-weight:bold;color:#003300">
+            <td>Admin Operations</td>
+        </tr>
+        <tr valign="top">
+            <td>
+                <ul>
+                    <li><asp:LinkButton ID="lnkUpdateBoxes" runat="server" Text="Update Boxes" onclick="lnkUpdateBoxes_Click" Font-Size="Large" ForeColor="#008000" /></li>
+                    <li><asp:LinkButton ID="lnkUpdateManifests" runat="server" Text="Update Manifests" onclick="lnkUpdateManifests_Click" Font-Size="Large" ForeColor="#008000" /></li>
+                    <li><asp:LinkButton ID="lnkUpdateUnknowns" runat="server" Text="Update Unknowns" onclick="lnkUpdateUnknowns_Click" Font-Size="Large" ForeColor="#008000" /></li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    <asp:Label ID="lblResult" runat="server" Font-Bold="True" Font-Size="Large" ForeColor="DarkGreen" />
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Clear" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="Footer" runat="server">
+</asp:Content>
---
+++ trunk/IMDBWeb/Secure/SPAKpages/SpakAdminActions.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace IMDBWeb.Secure.SPAKpages
+{
+    public partial class SpakAdminActions : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblErrMsg.Visible = false;
+                lblResult.Visible = false;
+            }
+        }
+
+        protected void lnkUpdateBoxes_Click(object sender, EventArgs e)
+        {
+            lblErrMsg.Visible = false;
+            lblResult.Visible = false;
+            lblResult.Text = string.Empty;
+            String spUpdateBox = "SPAK_Admin_IMDBBarcode_upd";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["IMDB_SQL"].ConnectionString;
+            SqlCommand cmdUpdateBox = new SqlCommand(spUpdateBox, con);
+            cmdUpdateBox.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            using (cmdUpdateBox)
+            {
+                try
+                {
+                    cmdUpdateBox.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    lblErrMsg.Visible = true;
+                    lblErrMsg.Text = ex.Message;
+                }
+                finally
+                {
+                    con.Close();
+                    lblResult.Visible = true;
+                    lblResult.Text = "Boxes in IMDB have been updated with SPAK values";
+                }
+            }
+        }
+        protected void lnkUpdateManifests_Click(object sender, EventArgs e)
+        {
+            lblErrMsg.Visible = false;
+            lblResult.Visible = false;
+            lblResult.Text = string.Empty;
+            String spUpdateManifest = "SPAK_Admin_IMDBManifest_upd";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["IMDB_SQL"].ConnectionString;
+            SqlCommand cmdUpdateManifest = new SqlCommand(spUpdateManifest, con);
+            cmdUpdateManifest.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            using (cmdUpdateManifest)
+            {
+                try
+                {
+                    cmdUpdateManifest.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    lblErrMsg.Visible = true;
+                    lblErrMsg.Text = ex.Message;
+                }
+                finally
+                {
+                    con.Close();
+                    lblResult.Visible = true;
+                    lblResult.Text = "Manifests in IMDB have been updated with SPAK values";
+                }
+            }
+        }
+        protected void lnkUpdateUnknowns_Click(object sender, EventArgs e)
+        {
+            lblErrMsg.Visible = false;
+            lblResult.Visible = false;
+            lblResult.Text = string.Empty;
+            String spUpdateUnknowns = "SPAK_Admin_SPAKBoxRcvd_FacilityProfile_upd";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["IMDB_SQL"].ConnectionString;
+            SqlCommand cmdUpdateUnknowns = new SqlCommand(spUpdateUnknowns, con);
+            cmdUpdateUnknowns.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            using (cmdUpdateUnknowns)
+            {
+                try
+                {
+                    cmdUpdateUnknowns.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    lblErrMsg.Visible = true;
+                    lblErrMsg.Text = ex.Message;
+                }
+                finally
+                {
+                    con.Close();
+                    lblResult.Visible = true;
+                    lblResult.Text = "UNKNOWNS in IMDB have been updated with SPAK values";
+                }
+            }
+        }
+    }
+}
