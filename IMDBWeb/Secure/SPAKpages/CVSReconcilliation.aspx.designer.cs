b2817a5a575b886709896a424997e791afaeaecb
bdaun1@gmail.com <bdaun1@gmail.com@db50c294-acef-6d43-bc94-4116f952d554>
2013-05-31 00:48:57 +0000

    Fix issue with recon submit button.

--- IMDBWeb/Secure/SPAKpages/CVSReconcilliation.aspx.designer.cs
+++ IMDBWeb/Secure/SPAKpages/CVSReconcilliation.aspx.designer.cs
@@ -157,15 +157,6 @@ namespace IMDBWeb.Secure.SPAKpages {
         protected global::System.Web.UI.WebControls.GridView gvBoxRecon;
         
         /// <summary>
-        /// tdReconBox control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlTableCell tdReconBox;
-        
-        /// <summary>
         /// lblReconBox control.
         /// </summary>
         /// <remarks>
