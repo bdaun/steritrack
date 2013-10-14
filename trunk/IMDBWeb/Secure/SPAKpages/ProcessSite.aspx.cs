2ff88f514777af223b18dca1bc7ad60597ab1ed0
bdaun1 <bdaun1@db50c294-acef-6d43-bc94-4116f952d554>
2013-10-14 04:43:34 +0000

    Add edit and insert capability

--- trunk/IMDBWeb/Secure/SPAKpages/ProcessSite.aspx.cs
+++ trunk/IMDBWeb/Secure/SPAKpages/ProcessSite.aspx.cs
@@ -11,20 +11,74 @@ namespace IMDBWeb.Secure.SPAKpages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            lblErrMsg.Visible = false;
+            lblMsg.Visible = false;
         }
 
         protected void btnNewSite_Click(object sender, EventArgs e)
         {
+            lblMsg.Visible = true;
+            lblMsg.Text = "Enter a unique Sitename and SiteCode and select if site should prompt for 10d warnings";
+            ddSitename.Visible = false;
+            btnNewSite.Visible = false;
+            lblSiteName.Visible = false;
             fvSite.ChangeMode(FormViewMode.Insert);
         }
         protected void UpdateCancel(object sender, EventArgs e)
         {
+            ddSitename.Items.Clear();
+            ddSitename.Items.Add(new ListItem("Select From List","0"));
+            ddSitename.DataBind();
             ddSitename.SelectedIndex = 0;
         }
         protected void InsertCancel(object sender, EventArgs e)
         {
+            ddSitename.Visible = true;
+            btnNewSite.Visible = true;
+            lblSiteName.Visible = true;
             ddSitename.SelectedIndex = 0;
+            fvSite.ChangeMode(FormViewMode.Edit);
+        }
+        protected void sdsSiteInfo_OnUpdating(Object sender, SqlDataSourceCommandEventArgs e)
+        {
+            e.Command.Parameters["@UserName"].Value = HttpContext.Current.User.Identity.Name.ToString();
+        }
+        protected void sdsSiteInfo_OnUpdated(object sender, EventArgs e)
+        {
+            lblMsg.Visible = true;
+            if (lblMsg.Text.ToString() == "Updated!")
+            {
+                lblMsg.Text = "Updated!!!";
+            }
+            else
+            {
+                lblMsg.Text = "Updated!";
+            }
+            ddSitename.Items.Clear();
+            ddSitename.Items.Add(new ListItem("Select from List", "0"));
+            ddSitename.DataBind();
+        }
+
+        protected void sdssiteInfo_OnInserting(Object sender, SqlDataSourceCommandEventArgs e)
+        {
+            e.Command.Parameters["@UserName"].Value = HttpContext.Current.User.Identity.Name.ToString();
+        }
+
+        protected void sdsSiteInfo_OnInserted(object sender, EventArgs e)
+        {
+            lblMsg.Visible = true;
+            lblMsg.Text = "Inserted!";
+            ddSitename.Items.Clear();
+            ddSitename.Items.Add(new ListItem("Select from List", "0"));
+            ddSitename.DataBind();
+            lblSiteName.Visible = true;
+            ddSitename.Visible = true;
+            btnNewSite.Visible = true;
+        }
+        protected void ddSitename_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblMsg.Visible = false;
+            lblErrMsg.Visible = false;
             fvSite.ChangeMode(FormViewMode.Edit);
         }
     }
